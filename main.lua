f0dd2508d379bb765809ce35b6bb943a50984308
Danil Korotenko <danil.korotenko@gmail.com>
2015-01-02 14:15:02 +0200

    Translated game to English.

--- main.lua
+++ main.lua
@@ -1,220 +1,222 @@
--- $Name:Волк, Коза и Капуста$
+-- $Name:Wolf, Goat and Cabbage$
 -- $Version:1.0.0$
 
 instead_version "1.3.0"
 
-game.act = 'Не получается.';
-game.inv = "Гм.. Не то..";
-game.use = 'Не сработает...';
+game.act = 'Action is undefined.';
+game.inv = "Inventory is undefined";
+game.use = 'Use is undefined';
 
-main = room {
-	nam = 'Волк, Коза и Капуста',
+main = room 
+{
+	nam = 'Wolf, Goat and Cabbage',
 	act = function() -- only one vobj, no check, just goto
 		goto('bankLeft');
 	end,
-	dsc = "Всем известная логческая игра."..[[^^
-	Реализация: Данил Коротенко (danil.korotenko@gmail.com).]],
-	obj = { vway("дальше", "Нажмите {здесь} чтобы начать игру.", 'bankLeft') }
+	dsc = "This is tha famous logical puzzle."..[[^^
+	Implementation: Danil Korotenko (danil.korotenko@gmail.com).]],
+	obj = { vway("next", "Click {here} to play.", 'bankLeft') }
 };
 
-wolf = obj {
-	nam = 'Волк',
-	dsc = 'Прирученый {волк} сидит недалеко от вас.',
+wolf = obj 
+{
+	nam = 'Wolf',
+	dsc = '{Wolf} is sitting near.',
 	act = function(s)
 		if inv():srch('boat') then
-			p 'Вы не можете подозвать к себе волка сидя в лодке.';
+			p 'You cannot call wolf when you in the boat.';
 		else
-			p 'Вы подозвали волка к себе.';
+			p 'You called wolf.';
 			inv():add('wolf');
 			objs():del('wolf');
 		end;
 	end,
 	inv = function(s)
-		p '"Гулять!" сказали вы волку.';
+		p '"Go for a walk!" - you commanded to wolf.';
 		objs():add(s);
 		inv():del(s);
 	end,
 	use = function(s, w)
 		if w ~= boat then
-			p 'Не полчится.'
-                        return false
+    		return false
 		else
 			if #boat.obj == 0 then
 				inv():del(wolf);
 				boat.obj:add('wolfOnBoat');
-				p 'Вы заводите волка в лодку.';
+				p 'You taked wolf to boat.';
 			else
-				p 'В лодке уже кто то сидит.'
-		                return false
+				p 'You cannot put in the boat more then one object.'
+		        return false
 			end
 		end
 	end
 };
 
-wolfOnBoat = obj {
-	nam = 'Волк',
-	dsc = 'Прирученый {волк} сидит в лодке.',
-	tak = 'Волк теперь смотрит на вас внимательно.',
+wolfOnBoat = obj 
+{
+	nam = 'Wolf',
+	dsc = '{Wолк} is sitting in the boat.',
+	tak = 'Wolf is looking at you carefully.',
 	inv = function(s)
 		if not inv():srch('boat') then
-			p 'Вы выводите волка на берег.';
+			p 'You bringed wolf to the river bank.';
 			objs():add('wolf');
 			inv():del('wolfOnBoat');
-			if objs():srch(kapusta) and objs():srch(goat) and objs():srch(wolf) then
+			if objs():srch(cabbage) and objs():srch(goat) and objs():srch(wolf) then
 				goto('gameEnd');
 			end;
 		else
-			p 'Вы не можете вывести волка на берег сидя в лодке.'
+			p 'You cannot bring wolf to the river bank when you in the boat.'
 		end
 	end,
 	use = function(s, w)
 		if w ~= boat then
-			p 'Не полчится.'
-                        return false
+            return false
 		else
 			inv():del(s);
 			boat.obj:add('wolfOnBoat');
-			p 'Вы заводите волка обратно в лодку.';
+			p 'You taked wolf to boat.';
 		end
 	end
 };
 
-goat = obj {
-	nam = 'Коза',
-	dsc = 'Рядом стоит {коза} и смотрит вдаль. На {козу} одет ошейник, к ошейнику привязана веревочка.',
+goat = obj 
+{
+	nam = 'Goat',
+	dsc = '{Goat} is staying near and it looking into the distance.',
 	act = function(s)
 		if inv():srch('boat') then
-			p 'Вы не можете взять козу за веревочку сидя в лодке.';
+			p 'You cannot get the goat when you in the boat.';
 		else
-			p 'Вы взяли козу за веревочку.';
+			p 'You got the goat.';
 			inv():add('goat');
 			objs():del('goat');
 		end;
 	end,
 	inv = function(s)
-		p 'Вы выводите козу на берег.';
+		p 'You bringed goat to the river bank.';
 		objs():add(s);
 		inv():del(s);
 	end,
 	use = function(s, w)
 		if w ~= boat then
-			p 'Не полчится.'
-                        return false
+            return false
 		else
 			if #boat.obj == 0 then
 				inv():del(goat);
 				boat.obj:add('goatOnBoat');
-				p 'Вы заводите козу в лодку.';
+				p 'You taked goat to the boat.';
 			else
-				p 'В лодке уже кто то сидит.'
-		                return false
+				p 'You cannot put in the boat more then one object.'
+                return false
 			end
 		end
 	end
 };
 
-goatOnBoat = obj {
-	nam = 'Коза',
-	dsc = 'В лодке стоит {Коза} и смотрит вдаль.',
-	tak = 'Вы взяли козу за веревочку.',
+goatOnBoat = obj 
+{
+	nam = 'Goat',
+	dsc = '{Goat} is staying in the boat and looking to the distance.',
+	tak = 'You got the goat.',
 	inv = function(s)
 		if not inv():srch('boat') then
-			p 'Вы выводите козу на берег.';
+			p 'You bringed goat to the river bank.';
 			objs():add('goat');
 			inv():del('goatOnBoat');
-			if objs():srch(kapusta) and objs():srch(goat) and objs():srch(wolf) then
+			if objs():srch(cabbage) and objs():srch(goat) and objs():srch(wolf) then
 				goto('gameEnd');
 			end;
 		else
-			p 'Вы не можете вывести козу на берег сидя в лодке.'
+			p 'You cannot bring the goat to the river bank when you in the boat.'
 		end
 	end,
 	use = function(s, w)
 		if w ~= boat then
-			p 'Не полчится.'
-                        return false
+			return false
 		else
 			inv():del(s);
 			boat.obj:add('goatOnBoat');
-			p 'Вы заводите козу обратно в лодку.';
+			p 'You taked goat to the boat.';
 		end
 	end
 };
 
-kapusta = obj {
-	nam = 'Капуста',
-	dsc = 'На земле лежит большой кочан {капусты}.',
+cabbage = obj 
+{
+	nam = 'Cabbage',
+	dsc = '{Cabbage} is laying on the ground.',
 	act = function(s)
 		if inv():srch('boat') then
-			p 'Вы не можете взять капусту с земли сидя в лодке.';
+			p 'You cannot take the cabbage when you in the boat.';
 		else
-			p 'Вы взяли капусту в руки.';
-			inv():add('kapusta');
-			objs():del('kapusta');
+			p 'You taked cabbage.';
+			inv():add('cabbage');
+			objs():del('cabbage');
 		end;
 	end,
 	inv = function(s)
-		p 'Вы кладете капусту на землю.';
+		p 'You putted cabbage to the ground.';
 		objs():add(s);
 		inv():del(s);
 	end,
 	use = function(s, w)
 		if w ~= boat then
-			p 'Не полчится.'
-                        return false
+            return false
 		else
 			if #boat.obj == 0 then
 				inv():del(s);
-				boat.obj:add('kapustaOnBoat');
-				p 'Вы кладете капусту в лодку.';
+				boat.obj:add('cabbageOnBoat');
+				p 'You put cabbage to the boat.';
 			else
-				p 'В лодке уже кто то сидит.'
-		                return false
+				p 'You cannot put in the boat more then one object.'
+		        return false
 			end
 		end
 	end
 };
 
-kapustaOnBoat = obj {
-	nam = 'Капуста',
-	dsc = 'В лодке лежит большой кочан {капусты}.',
-	tak = 'Вы взяли капусту в руки.',
+cabbageOnBoat = obj 
+{
+	nam = 'Cabbage',
+	dsc = '{Cabbage} is laying in the boat.',
+	tak = 'You got the cabbage.',
 	inv = function(s)
 		if not inv():srch('boat') then
-			p 'Вы кладете капусту на землю.';
-			objs():add('kapusta');
-			inv():del('kapustaOnBoat');
-			if objs():srch(kapusta) and objs():srch(goat) and objs():srch(wolf) then
+			p 'You put tha cabbage to the ground.';
+			objs():add('cabbage');
+			inv():del('cabbageOnBoat');
+			if objs():srch(cabbage) and objs():srch(goat) and objs():srch(wolf) then
 				goto('gameEnd');
 			end;
 		else
-			p 'Вы не можете положить капусту на землю сидя в лодке.'
+			p 'You cannot put the cabbage to the ground when you in the boat.'
 		end
 	end,
 	use = function(s, w)
 		if w ~= boat then
-			p 'Не полчится.'
-                        return false
+            return false
 		else
 			inv():del(s);
-			boat.obj:add('kapustaOnBoat');
-			p 'Вы кладете капусту обратно в лодку.';
+			boat.obj:add('cabbageOnBoat');
+			p 'You put the cabbage back to the boat.';
 		end
 	end
 };
 
-boat = obj {
-	nam = 'Лодка',
-	dsc = 'У берега стоит маленькая {лодочка}.',
+boat = obj 
+{
+	nam = 'Boat',
+	dsc = 'A little {boat} is near the river bank.',
 	act = function(s)
-		if inv():srch(kapusta) then
-			p 'Вы не можете залезть в лодку с капустой в руках.';
+		if inv():srch(cabbage) then
+			p 'You cannot got on the boat with cabbage in your hands.';
 		else 
 			if inv():srch(wolf) then
-				p 'Вы не можете залезть в лодку с волком одновременно.';
+				p 'You cannot got on the boat with the wolf at the same time.';
 			else 
 				if inv():srch(goat) then
-					p 'Вы не можете залезть в лодку с козой одновременно.';
+					p 'You cannot got on the boat with the goat at the same time.';
 				else
 					if where(me()) == bankLeft then
 						ways():add('bankRight');
@@ -228,35 +230,36 @@ boat = obj {
 							inv():add('boat');
 						end;
 					end;
-					p 'Вы сели в лодку.';
+					p 'You got on the boat.';
 				end;
 			end;
 		end;
 	end,
 	inv = function(s)
-		p 'Вы вышли из лодки.';
+		p 'you got out from the boat.';
 		inv():del(s);
 		objs():add(s);
 		ways():zap();
 	end
 };
 
-bankLeft = room {
-	nam = 'Берег Левый',
-	dsc = 'Это левый берег реки.',
+bankLeft = room 
+{
+	nam = 'Left Bank',
+	dsc = 'This is the left bank.',
 	enter = function(s, f)
 		if f == bankRight then
 			if objs(bankLeft):srch('wolf') then
 				if objs(bankLeft):srch('goat') then
-					p 'Волк сожрал козу';
+					p 'Wolf eat goat.';
 					objs(bankLeft):del(goat);
 					goto(gameOver);
 				end;
 			else
 				if objs(bankLeft):srch('goat') then
-					if objs(bankLeft):srch('kapusta') then
-						p 'Коза сожрала капусту';
-						objs(bankLeft):del(kapusta);
+					if objs(bankLeft):srch('cabbage') then
+						p 'Goat eat cabagge.';
+						objs(bankLeft):del(cabbage);
 						goto(gameOver);
 					end;
 				end;
@@ -268,26 +271,27 @@ bankLeft = room {
 function init()
 	bankLeft.obj:add('wolf');
 	bankLeft.obj:add('goat');
-	bankLeft.obj:add('kapusta');
+	bankLeft.obj:add('cabbage');
 	bankLeft.obj:add('boat');
 end
 
-bankRight = room {
-	nam = 'Берег Правый',
-	dsc = 'Это правый берег реки.',
+bankRight = room 
+{
+	nam = 'Right Bank',
+	dsc = 'Thi is the right bank.',
 	enter = function(s, f)
 		if f == bankLeft then
 			if objs(bankRight):srch('wolf') then
 				if objs(bankRight):srch('goat') then
-					p 'Волк сожрал козу';
+					p 'Wolf eat goat.';
 					objs(bankRight):del(goat);
 					goto(gameOver);
 				end;
 			else
 				if objs(bankRight):srch('goat') then
-					if objs(bankRight):srch('kapusta') then
-						p 'Коза сожрала капусту';
-						objs(bankRight):del(kapusta);
+					if objs(bankRight):srch('cabbage') then
+						p 'Goat eat cabagge.';
+						objs(bankRight):del(cabbage);
 						goto(gameOver);
 					end;
 				end;
@@ -296,12 +300,14 @@ bankRight = room {
 	end
 };
 
-gameOver = room {
-	nam = 'Миссия провалена.',
-	dsc = 'Игра закончена.',
+gameOver = room 
+{
+	nam = 'Mission failed.',
+	dsc = 'Game over.',
 }
 
-gameEnd = room {
-	nam = 'Миссия выполнена.',
-	dsc = 'Поздравляем! Вы успешно закончили игру.',
+gameEnd = room 
+{
+	nam = 'Mission completed.',
+	dsc = 'Congratulations!',
 }
